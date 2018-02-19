7938034dceeddc3ecad7e32fd14918518fb3ecfe
SimplicityDev2018 <simplicity2018@outlook.com>
2018-02-19 15:45:19 +0000

    changed package details in bitcoin-config.h
    changed package details in bitcoin-config.h   "Please do not build with autogen.sh it has not been fully implemented or tested yet"

--- src/config/bitcoin-config.h
+++ src/config/bitcoin-config.h
@@ -199,7 +199,7 @@
 /* #undef HAVE_WORKING_BOOST_SLEEP_FOR */
 
 /* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "info@bitcoin.org"
+#define PACKAGE_BUGREPORT "simplicity2018@outlook.com"
 
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "Simplicity"
@@ -208,13 +208,13 @@
 #define PACKAGE_STRING "Simplicity 2.0.0"
 
 /* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "bitcoin"
+#define PACKAGE_TARNAME "simplicity"
 
 /* Define to the home page for this package. */
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "3.0.1"
+#define PACKAGE_VERSION "2.0.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
