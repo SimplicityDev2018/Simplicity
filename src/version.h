750db69d68339ff09ea750ce58d37d000d0e89bc
ComputerCraftr <ComputerCraftr@users.noreply.github.com>
2018-04-04 19:44:04 -0700

    Don't connect to out of date MNs!
    This is what is causing darksend to crash!

--- src/version.h
+++ src/version.h
@@ -41,12 +41,12 @@ static const int PROTOCOL_VERSION = 70020;
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70016;
+static const int MIN_PEER_PROTO_VERSION = 70019;
 
 // minimum peer version accepted by DarkSendPool
-static const int MIN_POOL_PEER_PROTO_VERSION = 70016;
+static const int MIN_POOL_PEER_PROTO_VERSION = 70020;
 
-static const int MIN_INSTANTX_PROTO_VERSION = 70016;
+static const int MIN_INSTANTX_PROTO_VERSION = 70020;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
